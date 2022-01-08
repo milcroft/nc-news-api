a157d87e35d745b6ca4ac5870ad0875b9a72d6f8
Darren Nickerson <darren.nickerson@milcroft.com>
2022-01-08 06:33:33 +0000

    updated database connection

--- listen.js
+++ listen.js
@@ -2,7 +2,7 @@ const app = require('./app');
 
 const { PORT = 9090 } = process.env;
 const cors = require('cors');
-app.use(cors({ origin: '*' }));
+app.use(cors())
 
 app.listen(PORT, (err) => {
   if (err) throw err;
