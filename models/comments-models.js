0152f934351ae6cbd0648aae5686b7e257d08eb2
Darren Nickerson <darren.nickerson@milcroft.com>
2022-01-10 05:48:30 +0000

    updated comments delete

--- models/comments-models.js
+++ models/comments-models.js
@@ -1,29 +1,43 @@
 const db = require('../db/connection');
 
+// exports.deleteCommentsById = async (comment_id) => {
+//   // check for comment id
+//   const { rows: valid_id } = await db.query(
+//     `SELECT * FROM comments WHERE comment_id = $1`,
+//     [comment_id]
+//   );
+
+//   const validCommentId = valid_id;
+//   let isValidCommentId = false;
+
+//   if (validCommentId[0]) {
+//     isValidCommentId = true;
+//   }
+
+//   if (!isValidCommentId) {
+//     return Promise.reject({
+//       status: 404,
+//       msg: 'non existent ID',
+//     });
+//   }
+//   const { row } = await db.query(
+//     `DELETE FROM comments WHERE comment_id = $1;`,
+//     [comment_id]
+//   );
+//   return row;
+// };
+
 exports.deleteCommentsById = async (comment_id) => {
-  // check for comment id
-  const { rows: valid_id } = await db.query(
-    `SELECT * FROM comments WHERE comment_id = $1`,
-    [comment_id]
-  );
-  const validCommentId = valid_id;
-  let isValidCommentId = false;
-
-  if (validCommentId[0]) {
-    isValidCommentId = true;
-  }
-
-  if (!isValidCommentId) {
+  const response = await db
+    .query(`DELETE FROM comments WHERE comment_id = $1;`, [comment_id]);
+  if (response.rowCount === 0) {
     return Promise.reject({
       status: 404,
-      msg: 'non existent ID',
+      msg: 'No Comments',
     });
+  } else {
+    return;
   }
-  const { row } = await db.query(
-    `DELETE FROM comments WHERE comment_id = $1;`,
-    [comment_id]
-  );
-  return row;
 };
 
 exports.updateCommentById = async (comment_id, inc_votes) => {
